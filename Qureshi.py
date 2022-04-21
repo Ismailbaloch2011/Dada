5b5a9863d3e7066a41066710866970a07822d5eb
Mr Qureshi Tech <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2022-04-21 20:00:59 +0500

    Add files via upload
    First Install This Cammand : 
    ❤️🙂
    This Tool Is Paid❤️🙂
    
    pkg update 
    pkg upgrade 
    pkg install python 
    pkg install git 
    pip install requests 
    pip install mechanize 
    pip install bs4 
    pip install futures 
    git clone https://github.com/Mr-Qureshi-xd/Dada  
    cd Dada 
    python Qureshi.py

---
+++ Qureshi.py
@@ -0,0 +1,15 @@
+First Install This Cammand : 
+❤️🙂
+This Tool Is Paid❤️🙂
+
+pkg update 
+pkg upgrade 
+pkg install python 
+pkg install git 
+pip install requests 
+pip install mechanize 
+pip install bs4 
+pip install futures 
+git clone https://github.com/Mr-Qureshi-xd/Dada  
+cd Dada 
+python Qureshi.py
